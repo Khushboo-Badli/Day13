bb7e18f131a9b092b60451c54fbe43908e23468e
Khushboo-Badli <112501553+Khushboo-Badli@users.noreply.github.com>
2022-09-21 21:14:43 +0530

    Add files via upload

---
+++ Day7/Demo_Covariance_and_Contravariance/Demo_Covariance_and_Contravariance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Covariance_and_Contravariance
+{
+    class Program
+    {
+        public string GetString() {
+            return "sample string";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Implementing Covariance-----------");
+            //declaring covariance
+            object[] car = new string[10];  // object array hold the refernce of string array
+
+            //car[0] = 12;         // car object array is of string type
+            car[0] = "Mercedes";
+            Console.WriteLine( car[0]);
+
+            Console.WriteLine("----------Implementing delegate Covariance-----------");
+
+        }
+    }
+}
